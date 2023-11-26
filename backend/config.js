929e78471bd6c5414be36a0e3f71a44e1a168db1
anjeljayswal <anjeljayswal@gmail.com>
2023-11-26 23:17:07 +0530

    initial

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://anjalijayswal9910:8sHAfzXixmaw69oB@cluster0.5gmxqsw.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
+
+// mongodb+srv://anjalijayswal9910:<password>@cluster0.5gmxqsw.mongodb.net/?retryWrites=true&w=majority
