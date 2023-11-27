498672898bcd09cee7e73d4212b38ac978ac8e4e
anjeljayswal <anjeljayswal@gmail.com>
2023-11-27 08:30:06 +0530

    for deploy

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,10 @@
-export const PORT = process.env.PORT || 5555;
+import dotenv from 'dotenv';
 
-export const mongoDBURL =
-  'mongodb+srv://anjalijayswal9910:8sHAfzXixmaw69oB@cluster0.5gmxqsw.mongodb.net/?retryWrites=true&w=majority';
+dotenv.config();
+
+export const PORT = process.env.PORT || 5555;
+export const mongoDBURL = process.env.BASE_URL;
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
 
-// mongodb+srv://anjalijayswal9910:<password>@cluster0.5gmxqsw.mongodb.net/?retryWrites=true&w=majority
